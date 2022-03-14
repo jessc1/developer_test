3feb732582a1e0addb1dfff8a871376f46042e20
jessc1 <mmariajessica@gmail.com>
2022-03-14 15:45:11 -0300

    add survey test

--- take5/survey/tests.py
+++ take5/survey/tests.py
@@ -1,3 +1,15 @@
-from django.test import TestCase
+from django.test import TestCase, Client
+from django.urls import reverse
+from .models import Survey
+from django.contrib.auth.models import User
 
-# Create your tests here.
+
+class TestSurveyView(TestCase):
+    
+    def test_survey_list_view(self):
+        response = self.client.get(reverse('survey'))
+        self.assertEqual(response.status_code, 200)
+
+    
+
+
