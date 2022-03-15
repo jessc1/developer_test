4b79f1851d8a9062164c9b421575dbeb26e51371
jessc1 <50178378+jessc1@users.noreply.github.com>
2022-03-15 13:25:56 -0300

    add question views

--- take5/survey/views.py
+++ take5/survey/views.py
@@ -3,7 +3,7 @@ from django.views.generic import TemplateView
 from .models import Survey, SurveyQuestion, SurveyQuestionAlternative
 from .serializers import (SurveyListSerializer, SurveyDetailSerializer, 
     SurveyQuestionListSerializer, SurveyQuestionDetailSerializer, 
-    SurveyQuestionAlternativeListSerializer)
+    SurveyQuestionAlternativeListSerializer, SurveyQuestionAlternativeDetailSerializer)
 
 class SurveyListAPIView(generics.ListAPIView):
 
@@ -34,6 +34,24 @@ class SurveyCreateAPIView(generics.CreateAPIView):
     queryset = Survey.objects.all()
     serializer_class = SurveyDetailSerializer
 
+class SurveyUpdateAPIView(generics.RetrieveUpdateAPIView):
+    """
+    Update Survey
+    """
+        
+    lookup_field = "id"
+    queryset = Survey.objects.all()
+    serializer_class = SurveyDetailSerializer
+
+class SurveyDeleteAPIView(generics.DestroyAPIView):
+    """
+    delete the survey in the api
+    """
+      
+    lookup_field = "id"
+    queryset = Survey.objects.all()
+    
+
 class SurveyQuestionListAPIView(generics.ListAPIView):
     """
     Return Survey Question List in the API
@@ -50,13 +68,86 @@ class SurveyQuestionRetrieveAPIView(generics.RetrieveAPIView):
     queryset = SurveyQuestion.objects.all()
     serializer_class = SurveyQuestionDetailSerializer
 
+class SurveyQuestionCreateAPIView(generics.CreateAPIView):
+    """
+    Add a new survey question in the API
+    """    
+      
+    queryset = Survey.objects.all()
+    serializer_class = SurveyQuestionDetailSerializer
+
+
+class SurveyQuestionUpdateAPIView(generics.RetrieveUpdateAPIView):
+    """
+    Update survey question
+    """
+        
+    lookup_field = "id"
+    queryset = Survey.objects.all()
+    serializer_class = SurveyQuestionDetailSerializer
+
+
+class SurveyQuestionDeleteAPIView(generics.DestroyAPIView):
+    """
+    delete the survey in the api
+    """
+      
+    lookup_field = "id"
+    queryset = SurveyQuestion.objects.all()    
+
+    
+
 class SurveyQuestionAlternativeListAPIView(generics.ListAPIView):
     """
-    Return Survey Question List in the API
+    Return the alternative list in the API
     """
     
     queryset = SurveyQuestionAlternative.objects.all()
-    serializer_class = SurveyQuestionAlternativeListSerializer    
+    serializer_class = SurveyQuestionAlternativeListSerializer
+
+
+class SurveyQuestionAlternativeRetrieveAPIView(generics.RetrieveAPIView):
+    """
+    Return the alternative detail in the API 
+    """
+    lookup_field = "id"
+    queryset = SurveyQuestionAlternative.objects.all()
+    serializer_class = SurveyQuestionAlternativeDetailSerializer
+
+
+class SurveyQuestionAlternativeCreateAPIView(generics.CreateAPIView):
+    """
+    Add a new survey question in the API
+    """    
+      
+    queryset = Survey.objects.all()
+    serializer_class = SurveyQuestionAlternativeDetailSerializer
+
+
+class SurveyQuestionAlternativeUpdateAPIView(generics.RetrieveUpdateAPIView):
+    """
+    Update survey question
+    """
+        
+    lookup_field = "id"
+    queryset = Survey.objects.all()
+    serializer_class = SurveyQuestionAlternativeDetailSerializer
+
+class SurveyQuestionAlternativeDeleteAPIView(generics.DestroyAPIView):
+    """
+    delete the survey in the api
+    """
+      
+    lookup_field = "id"
+    queryset = SurveyQuestionAlternative.objects.all()    
+
+
+
+    
+
+
+
+  
 
 
 
