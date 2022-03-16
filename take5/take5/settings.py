9aaa88dd36def4e738c41dca063748d59a752c1b
jessc1 <50178378+jessc1@users.noreply.github.com>
2022-03-16 11:09:54 -0300

    Update settings.py

--- take5/take5/settings.py
+++ take5/take5/settings.py
@@ -20,6 +20,8 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/4.0/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
+
+#SECRET_KEY='django-insecure-4(^yla&t2y==r_=^h&r2q0sj9(o6r9d@a2-f-fnc3r4@%x6#a4'
 SECRET_KEY = os.environ.get("SECRET_KEY")
 
 # SECURITY WARNING: don't run with debug turned on in production!
